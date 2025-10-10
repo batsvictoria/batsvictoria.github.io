0ce7739395e8e50eb46e9eaab918d4e950d047af
Kimberley Lawrence <kimberleynedic@gmail.com>
2025-10-11 09:40:56 +1100

    calander changes

--- app/calender/page.tsx
+++ app/calender/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
                   <FontAwesomeIcon icon={faSun} className="text-black dark:text-white" size="3x" />
                 </div>
 
-                <h2 className="text-gray-900 dark:text-white text-2xl mb-2 text-center"> Morning</h2>
+                <h2 className="text-gray-900 dark:text-white text-2xl mb-2 text-center font-bold"> Morning</h2>
                 <p className="text-center text-sm">Pups maybe playing and fall. Please observe before rescue</p>
               </td>
 
@@ -34,8 +34,8 @@ export default function Page() {
                 </div>
 
 
-                <h2 className="text-gray-900 dark:text-white text-2xl mb-2 text-center"> Evening</h2>
-                <p className="text-center text-sm">Mums will crèche pups, please don’t remove without advice</p>
+                <h2 className="text-gray-900 dark:text-white text-2xl mb-2 text-center font-bold "> After 5pm</h2>
+                <p className="text-center text-sm">Mums will crèche pups. please don’t remove without advice</p>
               </td>
             </tr>
           </tbody>
@@ -71,25 +71,27 @@ export default function Page() {
             <tr className="bg-red-600 border-b text-center ">
               <td colSpan={3} className="text-lg font-bold">CRITICAL</td>
             </tr>
-
+{/* Oct-Nov*/}
             <tr className="bg-red-300 text-black border-b align-top ">
 
               <td className=" list-disc px-4 py-1 text-sm">
                 <td>
                   <li>Small pup (size of hand) on ground.</li>
-                  <li>Pup low in tree, vocalising, no adults around,no adults responding.</li>
+                  <li>Pup low in tree, vocalising, no adults around, no adults responding.</li>
                 </td>
               </td>
+              {/* Nov-Dec*/}
               <td className="list-disc border-s ps-4 px-4 py-1 border-gray-400 text-sm">
                 <td>
                   <li>Pup on ground.</li>
                   <li> Pup low in tree, vocalizing, no adults around, no adults responding.</li>
                 </td>
               </td>
+              {/* Jan-Feb*/}
               <td className="list-disc border-s ps-4 px-4 py-1 border-gray-400 text-sm">
 
                 <li>Pup on ground. </li>
-                <li>Pup low in tree, vocalizing, no adults around, no adults responding.</li>
+                <li>Pup low in tree, vocalizing, no adults around, no adults responding, not able to fly.</li>
 
               </td>
 
@@ -101,18 +103,18 @@ export default function Page() {
               <td colSpan={3} className="text-lg font-bold">URGENT</td>
             </tr>
 
+              {/* Oct-Nov*/}
             <tr className="bg-orange-300 text-black border-b align-top ">
-
               <td className="px-4 py-1 text-sm">
 
-                <li>Pup alone, vocalising, no adults around.</li>
+                <li>Pup alone and vocalising, with NO ADULTS around.</li>
               </td>
-
+{/* Nov-Dec*/}
               <td className="px-4 py-1 border-s ps-4 border-gray-400 text-sm">
                 <ul className="list-disc">
-                  <li>Pup alone, vocalizing, no adults around.</li>
-                  <li> Condition check:</li>
-                  <ul>
+                  <li>Pup alone in tree, vocalizing, no adults around, no adults responding.</li>
+                  <li> Condition check is critical here, look for:</li>
+                  <ul >
                     <li>Is their call hoarse?</li>
                     <li>Are they skinny?</li>
                     <li>Eyes bright?</li>
@@ -120,9 +122,11 @@ export default function Page() {
                   </ul>
                 </ul>
               </td>
+               {/* Jan-Feb*/}
               <td className="px-4 py-1 border-s ps-4 border-gray-400 text-sm">
                 <ul className="list-disc ">
-                  <li>Pup will be learning to fly</li>
+                  <li>Pups will be alone in trees as they are learning to fly</li>
+                  <li> Condition check is critical here, look for:</li>
                   <ul>
                     <li>Is their call hoarse?</li>
                     <li>How are they moving?</li>
@@ -133,28 +137,33 @@ export default function Page() {
 
             </tr>
 
-            <tr className="bg-green-600 border-b text-center">
+            <tr className="bg-blue-600 border-b text-center">
               <td colSpan={3} className="text-lg font-bold">NON-URGENT</td>
             </tr>
 
-            <tr className="bg-green-200 text-black border-b align-top ">
+            <tr className="bg-blue-200 text-black border-b align-top ">
 
+              {/* Oct-Nov*/}
               <td className=" list-disc px-4 py-1 text-sm">
                 <td>
-                  <li>Small pup (size of hand) on ground.</li>
-                  <li>Pup low in tree, vocalising, no adults around,no adults responding.</li>
+                  <li>Pup alone, not vocalizing and adults near by.</li>
+                  <li>Low in tree (mum may have accidentally dropped - monitor to make sure mum isn't trying to retrieve baby before intervention).</li>
                 </td>
               </td>
+               {/* Nov-Dec*/}
               <td className="list-disc px-4 py-1 border-s ps-4 border-gray-400 text-sm">
                 <td>
-                  <li>Pup on ground.</li>
-                  <li> Pup low in tree, vocalizing, no adults around, no adults responding.</li>
+                  <li>Pup alone, not vocalizing and adults nearby</li>
+                  
                 </td>
               </td>
+              {/* Jan-Feb*/}
               <td className="list-disc px-4 py-1 border-s ps-3 border-gray-400 text-sm">
-
-                <li>Pup on ground. </li>
-                <li>Pup low in tree, vocalizing, no adults around, no adults responding.</li>
+                <li>Pup alone, not vocalizing and adults near by </li>
+             
+                <li> <p className="font-bold underline text-red-600">Experienced rescuers only!</p>
+                  You must approach with care, as pups will attempt to fly and can fly into the river!
+                </li>
 
               </td>
 
